98da0c2477955ed7e70b27d8560c197c800b9def
J_W_I_ <jwalraven12@gmail.com>
2025-06-14 20:09:56 +1000

    Comments

--- Command Line Learning and Testing/main.swift
+++ Command Line Learning and Testing/main.swift
@@ -5,18 +5,20 @@
 //  Created by JWI Master on 14/6/2025.
 //  Please push to GitHub
 
+
+// Prime number Calculator
 var Div: Bool = false
-for num in 2...100 {
-  for num2 in 2..<num{
+for num in 2...100 { // Main Number Loop
+  for num2 in 2..<num{ // Divisor loop - 2..<num so that 1 and the number are excluded since primes can be divisible by those
     if num%num2 == 0{
-      Div = true
+      Div = true // if any num 2 that is within our range is perfectly divisible into num 1, we know it's not a prime
     }
   }
   if Div {
-    Div = false
+    Div = false // reset Div to false to continue the loop
   }
   else {
-    print("\(num)")
+    print("\(num)") // We've found a prime!
   }
 }
 
