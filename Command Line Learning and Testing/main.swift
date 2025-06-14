2f5ed23974769a1e9a5600d3a23ee7895dc50e5c
J_W_I_ <jwalraven12@gmail.com>
2025-06-14 19:37:50 +1000

    Initial Commit

---
+++ Command Line Learning and Testing/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Command Line Learning and Testing
+//
+//  Created by JWI Master on 14/6/2025.
+//
+
+var Div: Bool = false
+for num in 2...100 {
+  for num2 in 2..<num{
+    if num%num2 == 0{
+      Div = true
+    }
+  }
+  if Div {
+    Div = false
+  }
+  else {
+    print("\(num)")
+  }
+}
+
+
